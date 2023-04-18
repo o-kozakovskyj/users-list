e454beb1beb73e45bae35ea682d83a523c064b95
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2023-04-18 12:30:10 +0300

    base poj structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <Typography variant="h1">hello</Typography>
+      </header>
+    </div>
+  );
+}
+
+export default App;
